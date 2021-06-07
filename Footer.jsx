6e2cb37ec215700aefcfe7b08e180101cf2ab787
nirmalsimon <70945218+nirmalsimon@users.noreply.github.com>
2021-06-07 13:51:22 +0300

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Footer()
+{
+  var currYear = new Date().getFullYear();
+return(
+
+<footer>
+  <p>Copyright @ {currYear} nirmal</p>
+  </footer>
+);
+
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note()
+{
+return(
+
+<div className="note">
+  <h1>Javascript and React.js</h1>
+  <p>thank you so much sir for helping poor students like us to learn.</p>
+  </div>
+);
+
+}
+export default Note;
